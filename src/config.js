b7fcbdd5a3bfa04ba0481b80ec543e94182a912e
valarxu <7781862@qq.com>
2025-04-28 23:05:35 +0800

    update: 修改波动阈值

--- src/config.js
+++ src/config.js
@@ -13,8 +13,8 @@ module.exports = {
 
     // 交易参数
     POSITION_SIZE: 400,          // 每次开仓金额
-    MAX_POSITIONS: 50,           // 最大持仓数量
-    PRICE_CHANGE_THRESHOLD: 0.01, // 1%的价格变动阈值
+    MAX_POSITIONS: 100,           // 最大持仓数量
+    PRICE_CHANGE_THRESHOLD: 0.005, // 1%的价格变动阈值
     STOP_LOSS_THRESHOLD: 0.9,    // 止损阈值，价格低于均价的90%时止损
     TRADE_FEE_RATE: 0.0005,     // 交易手续费率，0.05%
 
